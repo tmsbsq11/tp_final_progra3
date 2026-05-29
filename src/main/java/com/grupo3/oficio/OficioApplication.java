e3176c58596ef98c31ceac9acdc4021b6c377c67
tmsbsq11 <tomascorrado2003@gmail.com>
2026-05-29 09:23:01 -0300

    Primer commit, Maven

---
+++ src/main/java/com/grupo3/oficio/OficioApplication.java
@@ -0,0 +1,13 @@
+package com.grupo3.oficio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OficioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OficioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo3/oficio/OficioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo3.oficio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OficioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
